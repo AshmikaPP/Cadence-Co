510e20d2a4b7daa26c6519d4281d8f388e47370b
Ashmika <ashmikapp2002@gmail.com>
2024-06-19 17:08:39 +0530

    updated

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,8 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDB=async()=>{
     try {
-        // const con=await mongoose.connect(process.env.MONGO_URI)
-        const con= await mongoose.connect("mongodb://127.0.0.1:27017/ash")
+        const con= await mongoose.connect(process.env.MONGO_URL, { dbName: 'ash' })
         // console.log(`Mongo connected: ${con.connection.hos}`)
     } catch (error) {
        console.log(error)
